aadeafad04c265cf884d3621ad5d893e6a97248f
Ata Waris <ata1waris55@gmail.com>
2026-07-01 14:00:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (52.49%) - LeetSync

--- 520-detect-capital/detect-capital.py
+++ 520-detect-capital/detect-capital.py
@@ -1,17 +1,7 @@
 class Solution(object):
     def detectCapitalUse(self, word):
-        upr=0
-        for ch in word:
-            if ch.isupper():
-                upr+=1
-        if upr==len(word):
-            return True
-        if upr==0:
-            return True
-        if word[0].isupper() and upr==1:
-            return True
-        else:
-            return False
+   
+        return word.isupper() or word.islower() or word.istitle()
 
 
 
