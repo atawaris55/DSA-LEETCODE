2dc319d38b5a6ab0d4cce38c65ac8a23ef5647c7
Ata Waris <ata1waris55@gmail.com>
2026-07-05 12:34:39 +0530

    Time: 23 ms (62.36%) | Memory: 12.9 MB (38.61%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.py
@@ -6,7 +6,8 @@
 
 class Solution(object):
     def deleteNode(self, node):
-        node.val=node.next.val
-        node.next=node.next.next
+        # node.val=node.next.val
+        # node.next=node.next.next
+        node.val, node.next = node.next.val, node.next.next
 
         
