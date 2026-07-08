80e988578afa4ab7fbda3d6d1b561b4a0aed91cc
Ata Waris <ata1waris55@gmail.com>
2026-07-08 13:37:55 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (78.72%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,12 +1,7 @@
 class Solution(object):
     def singleNumber(self, nums):
-        my_dict={}
+        ans=0
         for num in nums:
-            if num in my_dict:
-                my_dict[num]+=1
-            else:
-                my_dict[num]=0
-        for key,val in my_dict.items():
-            if val==0:
-                return key
+            ans=ans^num
+        return ans
 
