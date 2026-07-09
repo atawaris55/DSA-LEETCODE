7bf80ff0c9e046f20b778b8b64db9eebb8935e4a
Ata Waris <ata1waris55@gmail.com>
2026-07-09 18:27:31 +0530

    Time: 115 ms (56.77%) | Memory: 22.3 MB (20.00%) - LeetSync

---
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def matchPlayersAndTrainers(self, players, trainers):
+        players.sort()
+        trainers.sort()
+        count=0
+        i=0
+        j=0
+        while i<len(players) and j <len(trainers):
+            if players[i] <= trainers[j]:
+                count+=1
+                i+=1
+                j+=1
+            else:
+                j+=1
+        return count
+
+
