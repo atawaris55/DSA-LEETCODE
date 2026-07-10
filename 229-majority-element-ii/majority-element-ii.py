795b9275eefd032d5c7adb8b78f846d8a0f91fa4
Ata Waris <ata1waris55@gmail.com>
2026-07-10 20:00:08 +0530

    Time: 19 ms (16.24%) | Memory: 18.1 MB (17.11%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,14 +1,10 @@
 class Solution(object):
     def majorityElement(self, nums):
-        seen={}
+
         n=len(nums)//3
         res=[]
-        for num in nums:
-            if num in seen:
-                seen[num]+=1
-            else:
-                seen[num]=1
-        for k,val in seen.items():
+        element_count = Counter(nums)
+        for k,val in element_count.items():
             if val>n:
                 res.append(k)
         return res
