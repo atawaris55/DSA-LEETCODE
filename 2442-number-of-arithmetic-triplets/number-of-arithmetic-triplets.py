19cf5d27f8b53bace1214f0abee5107d7bfc3d16
Ata Waris <ata1waris55@gmail.com>
2026-07-11 15:35:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (91.52%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
@@ -1,8 +1,9 @@
 class Solution(object):
     def arithmeticTriplets(self, nums, diff):
         ans=0
+        seen=set(nums)
         for num in nums:
-            if num +diff in nums and num + 2*diff in nums:
+            if num +diff in seen and num + 2*diff in seen:
                 ans+=1
         return ans 
 
