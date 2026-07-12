3dd4878866d01edf216a50548b8778359b2a6cdc
Ata Waris <ata1waris55@gmail.com>
2026-07-12 14:05:07 +0530

    Time: 63 ms (25.40%) | Memory: 12.6 MB (7.94%) - LeetSync

---
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def checkArithmeticSubarrays(self, nums, l, r):
+        res=[]
+        n=len(nums)
+        for i in range(len(l)):
+            g=sorted(nums[l[i]:r[i]+1])
+            diff = g[1] - g[0] 
+            flag=True
+            for j in range(1,len(g)-1):
+                if g[j+1]-g[j]!=diff:
+                    flag=False
+                    break
+            res.append(flag)
+        return res
+
+
+       
