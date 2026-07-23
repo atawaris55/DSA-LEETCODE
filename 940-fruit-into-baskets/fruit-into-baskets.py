0d94235d211e5ccbf036819d619f8982fe2135e1
Ata Waris <ata1waris55@gmail.com>
2026-07-23 12:52:56 +0530

    Time: 262 ms (60.72%) | Memory: 16.8 MB (88.96%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def totalFruit(self, fruits):
+        n=len(fruits)
+        l=0
+        pick={}
+        max_len=0
+
+
+        for i in range(n):
+            pick[fruits[i]]=pick.get(fruits[i],0)+1
+            while len(pick)>2:
+                pick[fruits[l]]-=1
+                if pick[fruits[l]]==0:
+                    del pick[fruits[l]]
+                l+=1
+            max_len=max(max_len,i-l+1)
+        return max_len
+
+            
+            
+
