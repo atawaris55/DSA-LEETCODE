ca9336b39a9d95f0d8a483c0ac9462aacd67d22d
Ata Waris <ata1waris55@gmail.com>
2026-08-04 19:44:32 +0530

    Time: 15 ms (100.00%) | Memory: 19 MB (71.37%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.py
@@ -1,14 +1,11 @@
 class Solution(object):
     def findDuplicates(self, nums):
-        frq={}
         res=[]
-        for num in nums:
-            if num in frq:
-                frq[num]+=1
+        arr=[0]*(len(nums)+1)
+        for i in nums:
+            if arr[i]==0:
+               
+                arr[i]=1
             else:
-                frq[num]=1
-        for k,v in frq.items():
-            if v>1:
-                res.append(k)
+                res.append(i)
         return res
-        
