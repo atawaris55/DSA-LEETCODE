cf2beff0c148ff87c9de3c7729aac2ef90a4b46c
Ata Waris <ata1waris55@gmail.com>
2026-08-09 20:19:26 +0530

    Time: 6 ms (55.67%) | Memory: 28.8 MB (46.69%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.py
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -10,6 +10,6 @@ class Solution(object):
             return 0
         if root.val<low:
             return self.rangeSumBST(root.right,low,high)
-        elif root.val>high:
+        if root.val>high:
             return self.rangeSumBST(root.left,low,high)
         return (root.val+self.rangeSumBST(root.left,low,high)+self.rangeSumBST(root.right,low,high))
