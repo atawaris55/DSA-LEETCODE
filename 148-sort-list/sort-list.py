72c122741e6be20f4ef42d641418f055af4ebca2
Ata Waris <ata1waris55@gmail.com>
2026-09-04 19:19:06 +0530

    Time: 407 ms (77.63%) | Memory: 51 MB (25.11%) - LeetSync

---
+++ 148-sort-list/sort-list.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def sortList(self, head):
+        if head is None:
+            return head
+        if head.next is None:
+            return head
+        slow=head
+        fast=head.next
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        mid=slow.next
+        slow.next=None
+        left=self.sortList(head)
+        right=self.sortList(mid)
+
+        dummy = ListNode(0)
+        curr = dummy
+        
+
+        while left and right:
+            if left.val<right.val:
+                curr.next=left
+                left=left.next
+            else:
+                curr.next=right
+                right=right.next
+            curr=curr.next
+        if left:
+            curr.next=left
+        else:
+            curr.next=right
+        return dummy.next
+
