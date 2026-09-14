310d386bccb0904669544984da5b58418b381de0
Ata Waris <ata1waris55@gmail.com>
2026-09-14 19:28:53 +0530

    Time: 6 ms (85.93%) | Memory: 14.6 MB (33.52%) - LeetSync

---
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortArrayByParityII(self, nums):
+        j=1
+        for i in range(0,len(nums),2):
+            if nums[i]%2==0:
+                continue
+            while nums[j]%2!=0:
+                j+=2
+            nums[i],nums[j]=nums[j],nums[i]
+        return nums
