d7004434c0b3df2c73e28228242c26d27274ff15
Ata Waris <ata1waris55@gmail.com>
2026-09-19 15:40:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (60.79%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.py
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.py
@@ -2,8 +2,8 @@ class Solution(object):
     def sumOfUnique(self, nums):
         fq={}
         total=0
-        for i in range(len(nums)):
-            fq[nums[i]]=fq.get(nums[i],0)+1
+        for num in nums:
+            fq[num]=fq.get(num,0)+1
         for num in fq:
             if fq[num]==1:
                 total+=num
